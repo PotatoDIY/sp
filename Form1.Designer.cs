8e4798aeb9b591bcec159cc86ef04568e2dee779
土豆电工 <tudoudiangong@163.com>
2019-12-05 22:27:22 +0800

    ..

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Potato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonCOM = new System.Windows.Forms.Button();
+            this.comlist = new System.Windows.Forms.ComboBox();
+            this.baudlist = new System.Windows.Forms.ComboBox();
+            this.textin = new System.Windows.Forms.TextBox();
+            this.textout = new System.Windows.Forms.TextBox();
+            this.buttonTx = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCOM
+            // 
+            this.buttonCOM.Location = new System.Drawing.Point(12, 76);
+            this.buttonCOM.Name = "buttonCOM";
+            this.buttonCOM.Size = new System.Drawing.Size(165, 51);
+            this.buttonCOM.TabIndex = 0;
+            this.buttonCOM.Text = "open";
+            this.buttonCOM.UseVisualStyleBackColor = true;
+            this.buttonCOM.Click += new System.EventHandler(this.buttonCOM_Click);
+            // 
+            // comlist
+            // 
+            this.comlist.FormattingEnabled = true;
+            this.comlist.Location = new System.Drawing.Point(12, 12);
+            this.comlist.Name = "comlist";
+            this.comlist.Size = new System.Drawing.Size(165, 26);
+            this.comlist.TabIndex = 1;
+            this.comlist.SelectedIndexChanged += new System.EventHandler(this.comlist_SelectedIndexChanged);
+            // 
+            // baudlist
+            // 
+            this.baudlist.FormattingEnabled = true;
+            this.baudlist.Location = new System.Drawing.Point(12, 44);
+            this.baudlist.Name = "baudlist";
+            this.baudlist.Size = new System.Drawing.Size(165, 26);
+            this.baudlist.TabIndex = 2;
+            this.baudlist.SelectedIndexChanged += new System.EventHandler(this.buadlist_SelectedIndexChanged);
+            // 
+            // textin
+            // 
+            this.textin.Location = new System.Drawing.Point(183, 12);
+            this.textin.Multiline = true;
+            this.textin.Name = "textin";
+            this.textin.Size = new System.Drawing.Size(742, 414);
+            this.textin.TabIndex = 3;
+            // 
+            // textout
+            // 
+            this.textout.Location = new System.Drawing.Point(183, 432);
+            this.textout.Multiline = true;
+            this.textout.Name = "textout";
+            this.textout.Size = new System.Drawing.Size(612, 45);
+            this.textout.TabIndex = 4;
+            // 
+            // buttonTx
+            // 
+            this.buttonTx.Location = new System.Drawing.Point(801, 432);
+            this.buttonTx.Name = "buttonTx";
+            this.buttonTx.Size = new System.Drawing.Size(124, 45);
+            this.buttonTx.TabIndex = 5;
+            this.buttonTx.Text = "发送数据";
+            this.buttonTx.UseVisualStyleBackColor = true;
+            this.buttonTx.Click += new System.EventHandler(this.buttonTx_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 489);
+            this.Controls.Add(this.buttonTx);
+            this.Controls.Add(this.textout);
+            this.Controls.Add(this.textin);
+            this.Controls.Add(this.baudlist);
+            this.Controls.Add(this.comlist);
+            this.Controls.Add(this.buttonCOM);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PotatoSerialPort 土豆串口调试器  by 土豆电工";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCOM;
+        private System.Windows.Forms.ComboBox comlist;
+        private System.Windows.Forms.ComboBox baudlist;
+        private System.Windows.Forms.TextBox textin;
+        private System.Windows.Forms.TextBox textout;
+        private System.Windows.Forms.Button buttonTx;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Potato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String comName;//com名称
+        public int baud;//波特率
+        public int status;//状态
+        private SerialPort sp;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            status = 0;//非打开状态
+            //找到串口
+            foreach (string vPortName in SerialPort.GetPortNames())
+            {
+                comlist.Items.Add(vPortName);
+            }
+            //设置找到的第一个为默认串口
+
+            if (comlist.Items.Count > 0)
+            {
+                comlist.SelectedIndex = 0;
+                comName = comlist.SelectedItem.ToString();
+            }
+            else
+            {
+                buttonCOM.Enabled = false;
+            }
+            
+
+            //波特率设置
+            string[] bauds = { "9600","19200", "56000", "115200"};
+            baudlist.Items.AddRange(bauds);
+            //默认选择第一个，波特率为9600
+            baudlist.SelectedIndex = 0;
+            baud = 9600;
+
+            buttonCOM.Text = "已关闭";
+            sp = new SerialPort();
+            sp.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                this.Invoke((EventHandler)(delegate
+                {
+                    textin.AppendText(sp.ReadExisting());
+                }));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private void comlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //获得选择的COM名称
+            comName=comlist.SelectedItem.ToString();
+        }
+
+        private void buadlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //设置波特率
+            baud = int.Parse(baudlist.SelectedItem.ToString());
+        }
+
+        private void buttonCOM_Click(object sender, EventArgs e)
+        {
+            //打开或关闭
+            if (status==0)//关闭状态，则执行打开操作
+            {
+
+                OpenSP();
+            }
+            else//打开状态，执行关闭状态
+            {
+                buttonCOM.Text = "已关闭";
+                sp.Close();
+                status = 0;
+            }
+        }
+
+        private void OpenSP()
+        {
+            try
+            {
+                sp.PortName = comName;
+                sp.BaudRate = baud;
+                sp.DataBits = 8;
+                sp.StopBits = (StopBits)1;
+                sp.ReadTimeout = 5000;
+                sp.Open();
+                buttonCOM.Text = "已开启";
+                status = 1;
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.Message);
+                throw e;
+            }
+        }
+
+        private void buttonTx_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine(textout.Text);
+            textout.Text = "";
+        }
+    }
+}
